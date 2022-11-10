a0558f558f628a6a9cdf9ca6a92b8b74eaa3be2a
Zzemog <kikogomezalemany@gmail.com>
2022-11-10 11:26:30 +0100

    Movimiento slime, seguimiento cámara cinemachine y primeras animaciones

---
+++ ASlimeTale/Assets/Scripts/SlimeMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeMove : MonoBehaviour
+{
+    private CharacterController controller;
+    private float playerSpeed = 5.0f;
+    private float turnSpeed = 90.0f;
+    private Animator anim;
+    private Vector3 vel;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
+        vel = transform.forward * Input.GetAxis("Vertical") * playerSpeed;
+        controller.SimpleMove(vel);
+
+        if(vel != Vector3.zero)
+		{
+            anim.SetBool("moving", true);
+		} else
+		{
+            anim.SetBool("moving", false);
+		}
+    }
+}
