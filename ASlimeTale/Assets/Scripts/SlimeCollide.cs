8d3ef250fb690fec712ecbfec51666e40dc8ceaf
marcgf5 <45403823+marcgf5@users.noreply.github.com>
2022-11-12 11:38:45 +0100

    Escena reclutar y cambios de escena

---
+++ ASlimeTale/Assets/Scripts/SlimeCollide.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeCollide : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "Monster":
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Reclutar");
+                break;
+
+            case "Enemy":
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Combat");
+                break;
+
+            default:
+                break;
+        }
+
+    }
+}
