d2698f523de9d95440b89d2495a8d0f872361181
PabloSF <62628314+pabletefest@users.noreply.github.com>
2022-11-13 19:37:46 +0100

    DataManager for saving and loading data

--- ASlimeTale/Assets/Scripts/DataManager.cs
+++ ASlimeTale/Assets/Scripts/DataManager.cs
@@ -1,21 +1,70 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DataManager : MonoBehaviour
 {
-    //[SerializeField]
-    //private List<IMonsterInfo> a;
+    const string PLAYER_TEAM_KEY = "PlayerTeam";
+    const string MONSTER_LEVEL_KEY = "MonsterLevel";
+    const string MONSTER_HP_KEY = "MonsterCurrentHP";
+    const string MONSTER_MP_KEY = "MonsterCurrentMP";
+    const string MONSTER_EXP_KEY = "MonsterCurrentEXP";
+    const string MONSTER_SKILLS_KEY = "MonsterSkills";
+
+    [SerializeField]
+    private static List<MonsterInfo> monstersTeam;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        monstersTeam = new List<MonsterInfo>();
+
+        string playerTeam = PlayerPrefs.GetString(PLAYER_TEAM_KEY, "Slime");
+
+        foreach (var member in playerTeam.Split(','))
+        {
+            var monsterInfo = new MonsterInfo(member);
+
+            monsterInfo.level = (uint)PlayerPrefs.GetInt(FormatKeyString(MONSTER_LEVEL_KEY, member), 1);
+            monsterInfo.currentHP = (uint)PlayerPrefs.GetInt(FormatKeyString(MONSTER_HP_KEY, member), (int)monsterInfo.getMaxHP());
+            monsterInfo.currentMP = (uint)PlayerPrefs.GetInt(FormatKeyString(MONSTER_MP_KEY, member), (int)monsterInfo.getMaxMP());
+            monsterInfo.currentExp = (uint)PlayerPrefs.GetInt(FormatKeyString(MONSTER_EXP_KEY, member), 0);
+
+            string skillNames = PlayerPrefs.GetString(FormatKeyString(MONSTER_SKILLS_KEY, member), "Ataque");
+
+            foreach (var skillName in skillNames.Split(','))
+                monsterInfo.skills.Add(skillName, new SkillData(Resources.Load<SkillSO>(String.Format($"SO/Skills/{skillName}"))));
+
+            monstersTeam.Add(monsterInfo);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void SaveTeamMembersData()
     {
-        
+        List<string> teamNames = new List<string>();
+
+        foreach (var monsterInfo in monstersTeam)
+        {
+            string monsterName = monsterInfo.getName();
+            teamNames.Add(monsterName);
+            PlayerPrefs.SetInt(FormatKeyString(MONSTER_LEVEL_KEY, monsterName), (int)monsterInfo.level);
+            PlayerPrefs.SetInt(FormatKeyString(MONSTER_HP_KEY, monsterName), (int)monsterInfo.currentHP);
+            PlayerPrefs.SetInt(FormatKeyString(MONSTER_MP_KEY, monsterName), (int)monsterInfo.currentMP);
+            PlayerPrefs.SetInt(FormatKeyString(MONSTER_EXP_KEY, monsterName), (int)monsterInfo.currentExp);
+            
+            PlayerPrefs.SetString(FormatKeyString(MONSTER_SKILLS_KEY, monsterName), String.Join(",", monsterInfo.skills.Keys));
+        }
+
+        PlayerPrefs.SetString(PLAYER_TEAM_KEY, String.Join(",", teamNames));
+    }
+
+    public static string FormatKeyString(string key, string name)
+    {
+        return String.Format($"{key}_{name}");
+    }
+
+    void OnDestroy()
+    {
+        //SaveTeamMembersData();
     }
 }
---
+++ ASlimeTale/Assets/Scripts/MonsterInfo.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterInfo
+{
+    [SerializeField]
+    private MonsterSO monsterSo;
+
+    public uint level { get; set; }
+    public uint currentHP { get; set; }
+    public uint currentMP { get; set; }
+    public uint currentExp { get; set; }
+
+    public Dictionary<string, SkillData> skills 
+    {
+        get
+        {
+            if (skills == null)
+                return new Dictionary<string, SkillData>();
+
+            return skills;
+        }
+
+        set 
+        {
+            skills = value;
+        } 
+    }
+
+    public string getName() => monsterSo.monsterName;
+
+    public uint getMaxHP() => monsterSo.baseMaxHP + level;
+    public uint getMaxMP() => monsterSo.baseMaxMP + level;
+
+    public uint getAttack() => monsterSo.baseAttack + level;
+    public uint getDefense() => monsterSo.baseDefense + level;
+    public uint getMagic() => monsterSo.baseMagic + level;
+    public uint getResistance() => monsterSo.baseResistance + level;
+    public uint getSpeed() => monsterSo.baseSpeed + level;
+
+    public MonsterInfo(string monsterName)
+    {
+        // TODO: LOAD FROM CENTRALIZED LOADER
+        monsterSo = Resources.Load<MonsterSO>(string.Format($"SO/Monsters/{monsterName}"));
+    }
+}
