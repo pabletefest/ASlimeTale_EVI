c5ec886c163c256c926b131e89a81b746eb089e4
PabloSF <62628314+pabletefest@users.noreply.github.com>
2022-11-14 18:13:43 +0100

    Corrected issue with auto start timer

--- ASlimeTale/Assets/AutoStartTimer.cs
+++ ASlimeTale/Assets/AutoStartTimer.cs
@@ -7,10 +7,22 @@ public class AutoStartTimer : MonoBehaviour
 {
     private const float runoverTime = 5f;
     private float elapsedTime = 0f;
+    private Vector3 lastMousePos;
+
+    void Awake()
+    {
+        lastMousePos = Input.mousePosition;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        if (Vector3.Distance(Input.mousePosition, lastMousePos) > 0.1)
+        {
+            elapsedTime = 0f;
+            lastMousePos = Input.mousePosition;
+        }
+
         if (elapsedTime >= runoverTime)
             SceneManager.LoadScene("LLanuraAfable");
 
