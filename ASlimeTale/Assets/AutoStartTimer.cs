6ceacc8ff336c7709a395f79b8453715f6d17652
PabloSF <62628314+pabletefest@users.noreply.github.com>
2022-11-14 18:00:04 +0100

    5 secs runover timer added

---
+++ ASlimeTale/Assets/AutoStartTimer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AutoStartTimer : MonoBehaviour
+{
+    private const float runoverTime = 5f;
+    private float elapsedTime = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (elapsedTime >= runoverTime)
+            SceneManager.LoadScene("LLanuraAfable");
+
+        elapsedTime += Time.deltaTime;
+    }
+}
