0d8e3c2fc4423e82a7d0f6838659ee05e1d4f23e
marcgf5 <45403823+marcgf5@users.noreply.github.com>
2022-12-03 12:40:21 +0100

    Instanciar monstruos por codigo en combate

--- ASlimeTale/Assets/Scripts/CombatManager.cs
+++ ASlimeTale/Assets/Scripts/CombatManager.cs
@@ -5,15 +5,49 @@ using UnityEngine;
 public class CombatManager : MonoBehaviour
 {
     [SerializeField]
-    private List<ScriptableObject> players;
+    private List<MonsterSO> players;
     
     [SerializeField]
-    private List<ScriptableObject> enemies;
+    private List<EnemySO> enemies;
 
+    [SerializeField]
+    private GameObject PlayerOne;
+
+    [SerializeField]
+    private GameObject PlayerTwo;
+
+    [SerializeField]
+    private GameObject PlayerThree;
+
+    [SerializeField]
+    private GameObject PlayerFour;
+
+    [SerializeField]
+    private GameObject OnePlayerPosition;
+
+    [SerializeField]
+    private GameObject TwoPlayerPosition;
+
+    [SerializeField]
+    private GameObject ThreePlayerPosition;
+
+    [SerializeField]
+    private GameObject FourPlayerPosition;
+
+    [SerializeField]
+    private GameObject TESTPrefabSlime;
+
+    [SerializeField]
+    private GameObject TESTPrefabCactoro;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerOne = TESTPrefabSlime;
+        SlimeMove slimeMove = PlayerOne.GetComponent<SlimeMove>();
+        slimeMove.enabled = false;
+        Instantiate(PlayerOne, TwoPlayerPosition.transform.Find("PositionPlayer1").transform.position, TwoPlayerPosition.transform.Find("PositionPlayer1").transform.rotation);
+        PlayerTwo = TESTPrefabCactoro;
+        Instantiate(PlayerTwo, TwoPlayerPosition.transform.Find("PositionPlayer2").transform.position, TwoPlayerPosition.transform.Find("PositionPlayer2").transform.rotation);
     }
 
     // Update is called once per frame
