630363082a07175b7f4033586848c5033a3f8994
marcgf5 <45403823+marcgf5@users.noreply.github.com>
2022-12-10 11:48:39 +0100

    Menu batalla refactoring

--- ASlimeTale/Assets/Scripts/MenuBatalla.cs
+++ ASlimeTale/Assets/Scripts/MenuBatalla.cs
@@ -22,18 +22,17 @@ public class MenuBatalla : MonoBehaviour
     private List<Text> actionTexts;
 
     [SerializeField]
-    private Text habilidad1;
-
-    [SerializeField]
-    private Text habilidad2;
-
-    [SerializeField]
-    private Text habilidad3;
-
-    [SerializeField]
-    private Text habilidad4;
+    private List<Text> skillTexts;
 
     int currentAction = 0;
+    enum menuType
+    {
+        ACTION,
+        SKILL,
+        ITEM
+    }
+    menuType menuOpen = menuType.ACTION;
+
 
     // Start is called before the first frame update
     void Start()
@@ -44,79 +43,76 @@ public class MenuBatalla : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            if(currentAction == actionTexts.Count - 1)
+        if(menuOpen == menuType.ACTION) {
+            if (Input.GetKeyDown(KeyCode.DownArrow))
             {
-                currentAction = 0;
+                if (currentAction == actionTexts.Count - 1)
+                {
+                    currentAction = 0;
+                }
+                else
+                {
+                    currentAction += 1;
+                }
+                UpdateActionSelection(currentAction);
             }
-            else
+            if (Input.GetKeyDown(KeyCode.UpArrow))
             {
-                currentAction += 1;
+                if (currentAction == 0)
+                {
+                    currentAction = actionTexts.Count - 1;
+                }
+                else
+                {
+                    currentAction -= 1;
+                }
+                UpdateActionSelection(currentAction);
             }
-            UpdateActionSelection(currentAction);
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            if (currentAction == 0)
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                currentAction = actionTexts.Count - 1;
-            }
-            else
-            {
-                currentAction -= 1;
-            }
-            UpdateActionSelection(currentAction);
-        }
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            if (currentAction == 1)
-            {
-                actionSelector.SetActive(false);
-                skillSelector.SetActive(true);
-                Dictionary<string, SkillData> skills = DataManager.InstanceDB.getTeamMemberByName("Slime").Skills;
-                List<string> skillNames = new List<string>(skills.Keys);
-                int skillNumber = skillNames.Count;
-                if (skillNumber >= 4)
+                if (currentAction == 1)
                 {
-                    habilidad1.text = skillNames[0];
-                    habilidad2.text = skillNames[1];
-                    habilidad3.text = skillNames[2];
-                    habilidad4.text = skillNames[3];
+                    EnableActionSelection(false);
+                    EnableSkillSelection(true);
+                    SetSkillText();
+                    menuOpen = menuType.SKILL;
+                    currentAction = 0;
                 }
-                else if (skillNumber == 3)
+                if (currentAction == 4)
                 {
-                    habilidad1.text = skillNames[0];
-                    habilidad2.text = skillNames[1];
-                    habilidad3.text = skillNames[2];
-                    habilidad4.text = "";
+                    UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+                    Cursor.visible = true;
                 }
-                else if (skillNumber == 2)
-                {
-                    habilidad1.text = skillNames[0];
-                    habilidad2.text = skillNames[1];
-                    habilidad3.text = "";
-                    habilidad4.text = "";
-
-                }
-                else if (skillNumber == 1)
-                {
-                    habilidad1.text = skillNames[0];
-                    habilidad2.text = "";
-                    habilidad3.text = "";
-                    habilidad4.text = "";
-                }
-                else if (skillNumber == 0)
-                {
-                    habilidad1.text = "No hay habilidades desbloqueadas.";
-                }
-            }
-            if (currentAction == 4)
-			{
-                UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
-                Cursor.visible = true;
             }
         }
+        if(menuOpen == menuType.SKILL)
+        {
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                if (currentAction == actionTexts.Count - 1)
+                {
+                    currentAction = 0;
+                }
+                else
+                {
+                    currentAction += 1;
+                }
+                UpdateActionSelection(currentAction);
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                if (currentAction == 0)
+                {
+                    currentAction = actionTexts.Count - 1;
+                }
+                else
+                {
+                    currentAction -= 1;
+                }
+                UpdateSkillSelection(currentAction);
+            }
+        }
+        
     }
 
     public void UpdateActionSelection(int selectedAction)
@@ -133,6 +129,64 @@ public class MenuBatalla : MonoBehaviour
         }
     }
 
+    public void UpdateSkillSelection(int selectedAction)
+    {
+        for (int i = 0; i < actionTexts.Count; ++i)
+        {
+            if (i == selectedAction)
+            {
+                skillTexts[i].color = highlightedColor;
+            }
+            else
+            {
+                skillTexts[i].color = Color.black;
+            }
+        }
+    }
+
+    public void SetSkillText()
+    {
+        Dictionary<string, SkillData> skills = DataManager.InstanceDB.getTeamMemberByName("Slime").Skills;
+        List<string> skillNames = new List<string>(skills.Keys);
+        int skillNumber = skillNames.Count;
+        if (skillNumber >= 4)
+        {
+            skillTexts[0].text = skillNames[0];
+            skillTexts[1].text = skillNames[1];
+            skillTexts[2].text = skillNames[2];
+            skillTexts[3].text = skillNames[3];
+        }
+        else if (skillNumber == 3)
+        {
+            skillTexts[0].text = skillNames[0];
+            skillTexts[1].text = skillNames[1];
+            skillTexts[2].text = skillNames[2];
+            skillTexts[3].text = "";
+        }
+        else if (skillNumber == 2)
+        {
+            skillTexts[0].text = skillNames[0];
+            skillTexts[1].text = skillNames[1];
+            skillTexts[2].text = "";
+            skillTexts[3].text = "";
+
+        }
+        else if (skillNumber == 1)
+        {
+            skillTexts[0].text = skillNames[0];
+            skillTexts[1].text = "";
+            skillTexts[2].text = "";
+            skillTexts[3].text = "";
+        }
+        else if (skillNumber == 0)
+        {
+            skillTexts[0].text = "No hay habilidades desbloqueadas.";
+            skillTexts[1].text = "";
+            skillTexts[2].text = "";
+            skillTexts[3].text = "";
+
+        }
+    }
 
     public void EnableSkillSelection(bool enabled)
     {
