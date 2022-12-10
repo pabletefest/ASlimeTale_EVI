330e5f3af1a914ba39832b084875672fae74a5c8
marcgf5 <45403823+marcgf5@users.noreply.github.com>
2022-12-10 10:51:47 +0100

    Menú habilidades añadido

--- ASlimeTale/Assets/Scripts/MenuBatalla.cs
+++ ASlimeTale/Assets/Scripts/MenuBatalla.cs
@@ -22,7 +22,16 @@ public class MenuBatalla : MonoBehaviour
     private List<Text> actionTexts;
 
     [SerializeField]
-    private GameObject meteorShower;
+    private Text habilidad1;
+
+    [SerializeField]
+    private Text habilidad2;
+
+    [SerializeField]
+    private Text habilidad3;
+
+    [SerializeField]
+    private Text habilidad4;
 
     int currentAction = 0;
 
@@ -61,9 +70,46 @@ public class MenuBatalla : MonoBehaviour
         }
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            if (currentAction == 0)
+            if (currentAction == 1)
             {
-                meteorShower.SetActive(true);
+                actionSelector.SetActive(false);
+                skillSelector.SetActive(true);
+                Dictionary<string, SkillData> skills = DataManager.InstanceDB.getTeamMemberByName("Slime").Skills;
+                List<string> skillNames = new List<string>(skills.Keys);
+                int skillNumber = skillNames.Count;
+                if (skillNumber >= 4)
+                {
+                    habilidad1.text = skillNames[0];
+                    habilidad2.text = skillNames[1];
+                    habilidad3.text = skillNames[2];
+                    habilidad4.text = skillNames[3];
+                }
+                else if (skillNumber == 3)
+                {
+                    habilidad1.text = skillNames[0];
+                    habilidad2.text = skillNames[1];
+                    habilidad3.text = skillNames[2];
+                    habilidad4.text = "";
+                }
+                else if (skillNumber == 2)
+                {
+                    habilidad1.text = skillNames[0];
+                    habilidad2.text = skillNames[1];
+                    habilidad3.text = "";
+                    habilidad4.text = "";
+
+                }
+                else if (skillNumber == 1)
+                {
+                    habilidad1.text = skillNames[0];
+                    habilidad2.text = "";
+                    habilidad3.text = "";
+                    habilidad4.text = "";
+                }
+                else if (skillNumber == 0)
+                {
+                    habilidad1.text = "No hay habilidades desbloqueadas.";
+                }
             }
             if (currentAction == 4)
 			{
