f423d911a1e8691fe26a4fb3f0e8b6f3db5ef01b
Zzemog <kikogomezalemany@gmail.com>
2022-12-10 11:50:18 +0100

    Simple recruit started

---
+++ ASlimeTale/Assets/Scripts/RecruitManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecruitManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Provisional press enter to recruit
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            //TODO Add monster to team
+
+
+
+            UnityEngine.SceneManagement.SceneManager.LoadScene("LlanuraAfable");
+        }
+    }
+}
