a24ea417386dccaec84e5f633aa1cb8927b3c844
Zzemog <kikogomezalemany@gmail.com>
2022-12-12 11:01:01 +0100

    Personalized recruit messages

--- ASlimeTale/Assets/Scripts/RecruitManager.cs
+++ ASlimeTale/Assets/Scripts/RecruitManager.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class RecruitManager : MonoBehaviour
 {
     [SerializeField]
-    private GameObject recruitableMonster;
-
-    [SerializeField]
     private GameObject SpawnPoint;
 
     private string monsterName;
+
+    [SerializeField]
+    private Text dialogueText;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,7 @@ public class RecruitManager : MonoBehaviour
         monsterName = PlayerPrefs.GetString("RecruitableMonster");
 
         InstantiateMonster();
+        SetupDialogue();
     }
 
     // Update is called once per frame
@@ -44,4 +46,22 @@ public class RecruitManager : MonoBehaviour
         var monster = Resources.Load<MonsterSO>(string.Format($"SO/Monsters/{monsterName}"));
         Instantiate(monster.monsterPrefab, SpawnPoint.transform.position, SpawnPoint.transform.rotation);
     }
+
+    void SetupDialogue()
+	{
+		switch (monsterName)
+		{
+            case "Cactoro":
+                dialogueText.text = "Ándale limo, dame tacos al pastor y me uniré a ti.";
+                break;
+
+            case "Abelago":
+                dialogueText.text = "¿Unirme a ti? ¡Perfecto! Necesito un compañero para irme de aventuras. Bzzz";
+                break;
+
+            case "Mudfish":
+                dialogueText.text = "Puedo ir contigo pero vas a tener que regarme a menudo.";
+                break;
+        }
+	}
 }
