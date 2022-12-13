6cb032cbebc90e646b4271558e1a76ce749c3cfd
pabletefest <stemmerpablo@gmail.com>
2022-12-13 17:37:54 +0100

    Compiling error when building fixed

--- ASlimeTale/Assets/Scripts/CombatManager.cs
+++ ASlimeTale/Assets/Scripts/CombatManager.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
